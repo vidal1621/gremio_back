612325ace75d4a6270f9e7d4453a73e1d44a1b00
cmendoza1621 <cmendoza@gregario.cl>
2023-04-18 16:29:53 -0400

    first

---
+++ config.py
@@ -0,0 +1,32 @@
+# Statement for enabling the development environment
+DEBUG = True
+
+# Define the application directoryipconfig
+
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# Define the database - we are working with
+DATABASE_CONNECT_OPTIONS = {"database": 'escuela_futbol',
+                            "host": '186.64.122.205',
+                            "port": '5432',
+                            "user": 'gremio',
+                            "password": '1020'
+                            }
+
+THREADS_PER_PAGE = 2
+# HOST = '25.78.23.166'
+# HOST = '25.78.23.166'
+HOST = '0.0.0.0'
+PORT = 8889
+# Enable protection agains *Cross-site Request Forgery (CSRF)*
+CSRF_ENABLED = True
+
+# Use a secure, unique and absolutely secret key for
+# signing the data.
+CSRF_SESSION_KEY = "asdwerwe"
+
+# Secret key for signing cookies
+SECRET_KEY = "123sdwr"
+
