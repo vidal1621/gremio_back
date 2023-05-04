6696c12f1491b88bc915c23b5c02c3291dd8d77f
cmendoza1621 <cmendoza@gregario.cl>
2023-05-04 18:01:26 -0400

    new mods in proyect with blueprint

--- modules/mod_maestro/controllers.py
+++ modules/mod_maestro/controllers.py
@@ -8,6 +8,24 @@ import psycopg2.extras
 mod_maestro= Blueprint('maestro', __name__, url_prefix='/maestro')
 
 
+@mod_maestro.route('/maestro_pagos_alumnos_api', methods=['GET', 'POST'])
+def maestro_pagos_alumnos():
+    cursor = db.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    data = json.loads(request.data)
+    if request.method == 'GET':
+        sql_alumnos = "select * from alumnos where cod_usuario=%s"
+        cursor.execute(sql_alumnos, [data['cod_usuario']])
+        alumnos_asociados = cursor.fetchall()
+        return Response(response=json.dumps(alumnos_asociados, default=str), status=200,
+                        mimetype='application/json')
+    elif request.method == 'POST':
+        sql_alumnos = """select * from pagos where cod_alumno=%s"""
+        cursor.execute(sql_alumnos, [data['cod_alumno']])
+        maestro_pagos_usuario = cursor.fetchall()
+        return Response(response=json.dumps(maestro_pagos_usuario, default=str), status=200,
+                        mimetype='application/json')
+
+
 @mod_maestro.route('/maestro_pagos_api', methods=['GET','POST'])
 def maestro_pagos():
     cursor = db.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
@@ -28,3 +46,25 @@ def maestro_pagos():
         maestro_pagos_usuario = cursor.fetchall()
         return Response(response=json.dumps(maestro_pagos_usuario, default=str), status=200,
                         mimetype='application/json')
+
+
+@mod_maestro.route('/maestro_totales_api', methods=['POST'])
+def maestro_totales_api():
+    cursor = db.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    data = json.loads(request.data)
+    if request.method == 'POST':
+        sql_total_pagos = "select coalesce (sum(monto),0) monto from pagos where desc_pagos='Pagado' and extract(month from fecha_pago)=%s and extract(year from fecha_pago)=extract(year from now())"
+        cursor.execute(sql_total_pagos, [data['mes']])
+        total_pagos = cursor.fetchall()
+        sql_total_deuda = "select coalesce (sum(monto),0) monto from pagos where desc_pagos='Pendiente' and extract(month from fecha_pago)=%s and extract(year from fecha_pago)=extract(year from now())"
+        cursor.execute(sql_total_deuda, [data['mes']])
+        total_deuda = cursor.fetchall()
+        sql_total_alumnos = "select count(*) total_alumnos from alumnos"
+        cursor.execute(sql_total_alumnos)
+        total_alumnos = cursor.fetchall()
+        object = {
+            'total_pagos': total_pagos,
+            'total_deuda': total_deuda,
+            'total_alumnos': total_alumnos
+        }
+        return Response(response=json.dumps(object, default=str), status=200, mimetype='application/json')
