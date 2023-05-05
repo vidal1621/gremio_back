16abdd00db7693568d8c322a9cebc25c13f295e2
cmendoza1621 <cmendoza@gregario.cl>
2023-05-05 14:54:27 -0400

    new mods in proyect with blueprint

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from werkzeug.serving import WSGIRequestHandler
 
 if __name__ == '__main__':
     WSGIRequestHandler.protocol_version = "HTTP/1.1"
-    app.run(host=config.HOST, port=config.PORT, debug=True)
+    app.run(host=config.HOST, port=config.PORT, debug=False)
