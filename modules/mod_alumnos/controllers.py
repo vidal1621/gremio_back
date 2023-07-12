3b12e9ea819ebf461ad91f6c28ed491b738c3ea0
cmendoza1621 <cmendoza@gregario.cl>
2023-07-12 12:19:30 -0400

    pagos

--- modules/mod_alumnos/controllers.py
+++ modules/mod_alumnos/controllers.py
@@ -181,7 +181,7 @@ def registro_alumno_correo(destinatario, password):
         msg = MIMEMultipart('related')
         msg['From'] = fromaddr
         msg['To'] = to
-        msg['Subject'] = "Registro de usuario *GREMIO*"
+        msg['Subject'] = "Registro de usuario GREMIO"
         contenido_html = """
         <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
